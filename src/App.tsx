506067a45a51d5a445db4873a1345ee59004bb4b
OTonGitHub <purple@daraknet.cc>
2025-08-29 08:29:32 +0500

    ~ page 24

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const RTr = {
+  prefix: 'The Road To ',
+  title: 'React',
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function getTitle(type?: string) {
+  if (type === 'heading') return <h1>{RTr.prefix + RTr.title}</h1>;
+  return RTr.prefix + RTr.title;
 }
 
-export default App
+function App() {
+  return (
+    <>
+      {getTitle('heading')}
+      {getTitle()}
+
+      <p>
+        <label>
+          Text Box <br />
+          <input type='text'></input>
+        </label>
+      </p>
+
+      <p>
+        <label htmlFor='Date'>Date</label>
+        <br />
+        <input id='Date' type='date'></input>
+      </p>
+    </>
+  );
+}
+
+export default App;
