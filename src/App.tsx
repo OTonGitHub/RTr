400cfcc2f4071f0dd9a72c70b117ccb020702e23
OTonGitHub <purple@daraknet.cc>
2025-08-30 15:47:18 +0500

    finish page 25

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,46 @@ const RTr = {
   title: 'React',
 };
 
+const NirDobovizki = {
+  title: 'C# Concurrency',
+  pages: 350,
+};
+
+const StephenCleary = {
+  title: 'Concurrency in C# Cookbook',
+  pages: 250_000_000_000_000_00,
+  price: 23.55,
+};
+
+const RobertSedgewick = {
+  title: 'Algorithms',
+  pages: 10n,
+  price: 5.44,
+};
+
+const bookList = [NirDobovizki, StephenCleary, RobertSedgewick];
+
 function getTitle(type?: string) {
   if (type === 'heading') {
     return React.createElement('h1', null, `${RTr.prefix + RTr.title}`);
   }
-  return RTr.prefix + RTr.title;
+  return 'React + TypeScript';
+}
+
+function getBooks() {
+  return (
+    <ul>
+      {bookList.map((book) => (
+        <li>
+          <div>Title: {book.title}</div>
+          <div>Pages: {book.pages}</div>
+          <div>
+            Safe: <strong>{String(Number.isSafeInteger(book.pages))}</strong>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 function App() {
@@ -18,11 +53,11 @@ function App() {
     <>
       {getTitle('heading')}
       {getTitle()}
+      <p>{getBooks()}</p>
 
       <p>
         <label>
-          Text Box <br />
-          <input type='text'></input>
+          Text: <input type='text'></input>
         </label>
       </p>
 
